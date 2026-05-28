baee3c721e76a42b6882c5537e8db5a97f31603a
giovannidemartinog1975-pixel <giovannidemartinog1975@gmail.com>
2026-05-28 22:30:39 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ubbivjwsgqnwuxswxejm.supabase.co'
-const supabaseAnonKey = 'incolla_qui_la_tua_chiave_anon'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InViYml2andzZ3Fud3V4c3d4ZWptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MDkxMDMsImV4cCI6MjA5NTI4NTEwM30.t4ppGrp6TH4Nq0kWlWzRCkHiKkWftPF3Iv_4S3mB5LA'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
