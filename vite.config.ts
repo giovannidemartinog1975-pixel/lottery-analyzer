ae763bef68b2b30fe427a558174524762156011e
giovannidemartinog1975-pixel <giovannidemartinog1975@gmail.com>
2026-06-12 10:50:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
+build: {
     target: 'esnext',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+      }
+    }
   },
 })
